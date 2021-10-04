7bc02c55c63057c74a4965120d7f951a0ff15918
Theo Marinho <theo.marinho@epitech.eu>
2021-10-04 15:41:13 +0200

    [fix] fix error on spinners svg

--- src/component/Spinner.jsx
+++ src/component/Spinner.jsx
@@ -4,9 +4,9 @@ const Spinner = ({color = "#fff", size = 48, classname}) => {
   return (
     // By Sam Herbert (@sherb), for everyone. More @ http://goo.gl/7AJzbL
     <svg width={size} height={size} viewBox="0 0 38 38" xmlns="http://www.w3.org/2000/svg" stroke={color}>
-      <g fill="none" fill-rule="evenodd">
+      <g fill="none" fillRule="evenodd">
         <g transform="translate(1 1)" strokeWidth=".7">
-          <circle stroke-opacity=".3" cx="18" cy="18" r="18"/>
+          <circle strokeOpacity=".3" cx="18" cy="18" r="18"/>
           <path d="M36 18c0-9.94-8.06-18-18-18">
             <animateTransform
               attributeName="transform"
